651f6bb2110edad12a06d30188a4f00225246b0e
Swati Singh1 <swati_singh1@epam.com>
2025-03-03 12:30:24 +0530

    wildcard error handling

--- src/app.js
+++ src/app.js
@@ -2,25 +2,49 @@ const express = require("express");
 
 const app = express();
 
-const { adminAuth, userAuth } = require("./middlewares/auth");
+// const { adminAuth, userAuth } = require("./middlewares/auth");
 
-app.use("/admin", adminAuth);
-// app.use("/user", userAuth);
+// app.use("/admin", adminAuth);
+// // app.use("/user", userAuth);
 
-app.get("/user/login", (req, res) => {
-  res.send("User Logged In!");
+// app.get("/user/login", (req, res) => {
+//   res.send("User Logged In!");
+// });
+
+// app.get("/user/data", userAuth, (req, res) => {
+//   res.send("User Data Sent!");
+// });
+
+// app.get("/admin/getAllData", (req, res) => {
+//   res.send("All Data Sent!");
+// });
+
+// app.get("/admin/deleteUser", (req, res) => {
+//   res.send("Deleted a user");
+// });
+
+app.use("/", (err, req, res, next) => {
+  if (err) {
+    // Log your error
+    res.status(500).send("Something went wrong");
+  }
 });
 
-app.get("/user/data", userAuth, (req, res) => {
+app.get("/getUserData", (req, res) => {
+  //   try {
+  // Logic of DB call and get user data
+  throw new Error("Error in fetching user data");
   res.send("User Data Sent!");
+  //   } catch (err) {
+  //     res.status(500).send("Something went wrong");
+  //   }
 });
 
-app.get("/admin/getAllData", (req, res) => {
-  res.send("All Data Sent!");
-});
-
-app.get("/admin/deleteUser", (req, res) => {
-  res.send("Deleted a user");
+app.use("/", (err, req, res, next) => {
+  if (err) {
+    // Log your error
+    res.status(500).send("Something went wrong");
+  }
 });
 
 app.listen(7777, () => {
