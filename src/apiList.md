a356e329d41d9e87e945d30c5ecf82b184652d75
Swati Singh1 <swati_singh1@epam.com>
2025-03-06 11:13:50 +0530

    feed API & pagination

--- src/apiList.md
+++ src/apiList.md
@@ -15,14 +15,12 @@
 ## connectionRequestRouter
 
 - POST /request/send/:status/:userId
-
-- POST /request/review/accepted/:requestId
-- POST /request/review/rejected/:requestId
+- POST /request/review/:status/:requestId
 
 ## userRouter
 
+- GET /user/requests/recieved
 - GET /user/connections
-- GET /user/requests
 - GET /feed - Gets you the profiles of other users on platform
 
 Status: ignore, interested, accepted, rejected
---
+++ src/routes/user.js
@@ -0,0 +1,96 @@
+const express = require("express");
+const userRouter = express.Router();
+const { userAuth } = require("../middlewares/auth");
+const ConnectionRequest = require("../models/connectionRequest");
+const { GiCarDoor } = require("react-icons/gi");
+const { connection } = require("mongoose");
+const User = require("../models/user");
+
+const USER_SAFE_DATA = "firstName lastName photoUrl age gender about skills";
+
+// Get all the pending connection requests for the loggedIn user
+userRouter.get("/user/requests/recieved", userAuth, async (req, res) => {
+  try {
+    const loggedInUser = req.user;
+    const connectionRequests = await ConnectionRequest.find({
+      toUserId: loggedInUser._id,
+      status: "interested",
+      // }).populate("fromUserId", ["firstName", "lastName", "photoUrl", "age", "gender", "about", "skills"]);
+    }).populate("fromUserId", USER_SAFE_DATA);
+
+    res.json({
+      message: "Data fetched Scuccessfully!",
+      data: connectionRequests,
+    });
+  } catch (err) {
+    res.status(400).send("ERROR: " + err.message);
+  }
+});
+
+userRouter.get("/user/connections", userAuth, async (req, res) => {
+  try {
+    const loggedInUser = req.user;
+    const connectionRequests = await ConnectionRequest.find({
+      $or: [
+        { toUserId: loggedInUser._id, status: "accepted" },
+        { fromUserId: loggedInUser._id, status: "accepted" },
+      ],
+    })
+      .populate("fromUserId", USER_SAFE_DATA)
+      .populate("toUserId", USER_SAFE_DATA);
+
+    const data = connectionRequests.map((row) => {
+      if (row.fromUserId._id.toString() === loggedInUser._id.toString()) {
+        return row.toUserId;
+      }
+      return row.fromUserId;
+    });
+
+    res.json({
+      message: "Data fetched Scuccessfully!",
+      data: data,
+    });
+  } catch (err) {
+    res.status(400).send("ERROR: " + err.message);
+  }
+});
+
+userRouter.get("/feed", userAuth, async (req, res) => {
+  try {
+    // user should see all the cards except
+    // his own card, his connections, ignored people, already sent the connection request
+    const loggedInUser = req.user;
+
+    const page = parseInt(req.query.page) || 1;
+    let limit = parseInt(req.query.limit) || 10;
+    limit = limit > 50 ? 50 : limit;
+    const skip = (page - 1) * limit;
+
+    // Find all the connection requests(sent+recieved)
+    const connectionRequests = await ConnectionRequest.find({
+      $or: [{ fromUserId: loggedInUser._id }, { toUserId: loggedInUser._id }],
+    }).select("fromUserId toUserId");
+
+    const hideUsersFromFeed = new Set();
+    connectionRequests.forEach((req) => {
+      hideUsersFromFeed.add(req.fromUserId.toString());
+      hideUsersFromFeed.add(req.toUserId.toString());
+    });
+
+    const users = await User.find({
+      $and: [
+        { _id: { $nin: Array.from(hideUsersFromFeed) } },
+        { _id: { $ne: loggedInUser._id } },
+      ],
+    })
+      .select(USER_SAFE_DATA)
+      .skip(skip)
+      .limit(limit);
+
+    res.json({ data: users });
+  } catch (err) {
+    res.status(400).send("ERROR: " + err.message);
+  }
+});
+
+module.exports = userRouter;
